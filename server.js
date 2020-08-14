0f970a6a2509d7d85d15bb319dc6fc702e013c42
bryan <bryanjm96@gmail.com>
2020-08-14 16:56:23 -0500

    aplicacion de prueba de login 2

--- server.js
+++ server.js
@@ -10,12 +10,15 @@ dotenv.config({
 app.listen(port, () => {
     console.log(`escuchando peticiones por el puerto: ${port}`);
 });
+if (process.env.NODE_ENV == "production") {
 
-mongoose.connect(process.env.LOCAL_DATABASE, {
+}
+
+mongoose.connect(process.env.CLOUD_DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false,
 }).then(() => {
-    console.log("conexion a base de datos local exitosa");
+    console.log("conexion a base de datos en la nube exitosa");
 }).catch(err => console.log(err));
